cd44c4d6e4c3453e303f2b21ebf5fc9761c17862
DeVontae316 <DeVontaeMoore316@yahoo.com>
2021-09-13 10:38:58 -0500

    Deleted node api function

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,5 @@
 const path = require("path")
 
-exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    })
-  }
-}
-
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const blogPostTemplate = path.resolve(`src/templates/blog.js`)
